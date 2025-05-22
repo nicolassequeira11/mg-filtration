870dd4788f9064c551607cb5c270a6ce18316b50
nicolassequeira11 <nicosequeira1997@hotmail.com>
2025-05-22 21:52:03 +0300

    new video banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,14 +1,33 @@
-import BannerHome from "../../assets/banner-2.jpg"
+import VideoBanner from "../../assets/video-banner-mg-filtration.mp4"
+import { useTranslation } from "react-i18next"
 
 export const Bannerhome = () => {
+  const {t} = useTranslation();
+
   return(
-    <div className="">
-      <div className="h-[75vh] max-md:h-[30vh] flex">
-        <img 
-          src={BannerHome} 
-          alt="" 
-          className="object-cover object-bottom w-full max-md:object-center"
-        />
+    <div className="flex w-full relative">
+
+      {/* Video */}
+      <video
+        src={VideoBanner}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full h-[80vh] max-md:h-[40vh] object-cover brightness-75 object-center max-md:object-center"
+      />
+
+      {/* Text */}
+      <div 
+        className="absolute left-0 right-0 bottom-0 top-0 text-center flex 
+          justify-center flex-col w-11/12 mx-auto"
+      >
+        <p className="text-5xl max-md:text-2xl text-white font-raleway font-bold">
+          {t("banner.title")}
+        </p>
+        <p className="text-2xl max-md:text-lg text-white mt-6 font-raleway">
+          {t("banner.subtitle")}
+        </p>
       </div>
     </div>
   )
