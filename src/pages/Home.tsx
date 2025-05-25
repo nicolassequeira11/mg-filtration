ae6e9e6c520d84ba71a0c86ab9eac3abb7cf5fe1
nicolassequeira11 <nicosequeira1997@hotmail.com>
2025-05-25 20:17:29 +0300

    helmet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { Bannerhome } from "../components/home/Banner"
 import { AboutUsHome } from "../components/home/AboutUs"
 import { IndustriesHome } from "../components/home/Industries"
@@ -7,13 +8,21 @@ import { PlantsHome } from "../components/home/Plants"
 
 export const Home = () => {
   return(
-    <div className="">
-      <Bannerhome />
-      <AboutUsHome />
-      <WhatWeDoHome />
-      <IndustriesHome />
-      <WhyHome />
-      <PlantsHome />
-    </div>
+    <>
+      <Helmet>
+        <title>M.G. Filtration Technology | Soluciones de filtración por membrana y tratamiento de agua</title>
+        <meta name="description" content="¿Buscas soluciones eficaces para el tratamiento de agua industrial? En M.G. Filtration Technology desarrollamos plantas a medida con tecnología de filtración por membranas." />
+        <link rel="canonical" href="https://www.mgfiltration.com/" />
+      </Helmet>
+
+      <section className="">
+        <Bannerhome />
+        <AboutUsHome />
+        <WhatWeDoHome />
+        <IndustriesHome />
+        <WhyHome />
+        <PlantsHome />
+      </section>
+    </>
   )
 }
